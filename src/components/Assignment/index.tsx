6a42f94ef4bffde3837b2c0ba5310f6d7cb51f7b
Monalisa Sarkar <monalisa.sarkar@hmrc.gov.uk>
2023-05-26 12:51:35 +0100

    BUG-6220 fix

--- src/components/Assignment/index.tsx
+++ src/components/Assignment/index.tsx
@@ -164,7 +164,7 @@ export default function Assignment(props) {
   }
 
   function scrollToTop(){
-    const position = document.querySelector('h1')?.offsetTop || 0;
+    const position = document.querySelector('a')?.offsetTop || 0;
     document.body.scrollTop = position;
     document.documentElement.scrollTop = position;
   }
