78bca422bf1573862ce337020695b05d324b5d27
Akhila <akhila.mineendsouza@hmrc.gov.uk>
2023-07-17 21:32:02 +0100

    Fixes

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,19 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
   .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     lng: sessionStorage.getItem('rsdk_locale')?.substring(0,2) || 'en',
     backend: {
@@ -23,9 +14,6 @@ i18n
     },
     fallbackLng: 'en',
     debug: true,
-    /* can have multiple namespace, in case you want to divide a huge translation into smaller pieces and load them on demand */
-    /* ns: ['ChB'],
-    defaultNS: 'ChB', */
     keySeparator: false,
     interpolation: {
       escapeValue: false,
