cd6ce7d2b5ae615193fe8246aa6914ffc588543b
monalisasarkar123 <monalisa.sarkar@hmrc.gov.uk>
2023-07-18 17:32:59 +0100

    Updated the text content

--- src/applications/ChildBenefitsClaim/index.tsx
+++ src/applications/ChildBenefitsClaim/index.tsx
@@ -14,6 +14,9 @@ import UserPortal from './UserPortal';
 import ClaimsList from '../../components/templates/ClaimsList';
 import setPageTitle from '../../helpers/setPageTitleHelpers';
 import LogoutPopup from '../../components/forms/LogoutPopup';
+import { useTranslation } from 'react-i18next';
+const { t } = useTranslation();
+
 
 // declare var gbLoggedIn: boolean;
 // declare var login: Function;
@@ -396,7 +399,7 @@ export default function ChildBenefitsClaim() {
         { /* !! NEEDS TRANSLATING  -- title & button content */ }
         <ClaimsList thePConn={pConn}
          data={inprogressClaims}
-         title='Claims in progress'
+         title=  {t("CLAIMS_IN_PROGRESS")}
          options={[{name:'Claim.Child.pyFirstName', label:'Childs\' name'}, {name:'Claim.Child.pyLastName'}, {name:'pyStatusWork'}, {name:'pxCreateDateTime', type:'Date', label:'claim created date'}, {name:'pyID', label:'claim reference'}]}
          loading={loadinginProgressClaims}
          rowClickAction="OpenAssignment"
@@ -408,7 +411,7 @@ export default function ChildBenefitsClaim() {
             buttonMetadata = lastName ? `${firstName} ${lastName}` : firstName;
           }
           return (
-           <>Continue claim <span className="govuk-visually-hidden"> for {buttonMetadata}</span></>
+           <>  {t("CONTINUE_CLAIM")} <span className="govuk-visually-hidden"> for {buttonMetadata}</span></>
            )
           }}
         />
@@ -416,7 +419,7 @@ export default function ChildBenefitsClaim() {
         {/* !! NEEDS TRANSLATING  -- title & button content */}
         <ClaimsList thePConn={pConn}
           data={submittedClaims}
-          title='Submitted claims'
+          title=  {t("SUBMITTED_CLAIMS")}
           options={[{name:'Claim.Child.pyFirstName', label:'Child\'s name'}, {name:'Claim.Child.pyLastName'}, {name:'pyStatusWork'}, {name:'pxCreateDateTime', type:'Date', label:'claim created date'}, {name:'pyID', label:'claim reference'}]}
           loading={loadingsubmittedClaims}
           rowClickAction="OpenCase"
@@ -427,7 +430,7 @@ export default function ChildBenefitsClaim() {
             if(firstName){
               buttonMetadata = lastName ? `${firstName} ${lastName}` : firstName;
             }
-            return <>View claim {buttonMetadata && <span className="govuk-visually-hidden"> for {buttonMetadata}</span>}</>
+            return <>   {t("VIEW_CLAIM")}  {buttonMetadata && <span className="govuk-visually-hidden"> for {buttonMetadata}</span>}</>
           }
           }
         />
