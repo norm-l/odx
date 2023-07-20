a91ed945cd6151f474001735834123a07aa95721
monalisasarkar123 <monalisa.sarkar@hmrc.gov.uk>
2023-07-20 10:56:42 +0100

    Removing Portal Helplinenumbers

--- src/applications/ChildBenefitsClaim/UserPortal.tsx
+++ src/applications/ChildBenefitsClaim/UserPortal.tsx
@@ -17,22 +17,6 @@ export default function UserPortal(props){
         {children}
       </div>
       <div className='govuk-grid-column-one-third'>
-        <p className='govuk-body'>
-          Call the Child Benefit helpline if you need to speak with us or use the chat
-        </p>
-        <p className='govuk-body'>You&apos;ll need:</p>
-        <ul className='govuk-list govuk-list--bullet'>
-          <li>National Insurance number</li>
-        </ul>
-        <ul className='govuk-details__text govuk-list'>
-          <li>Child Benefit helpline</li>
-          <li>Telephone: 0300 200 3100</li>
-          <li>Welsh language: 0300 200 1900</li>
-          <li>Textphone: 0300 200 3103 </li>
-          <li>Outside UK: +44 161 210 3086</li>
-          <li>Monday to Friday, 8am to 6pm</li>
-        </ul>
-
         <span className='govuk-heading-m'>Online</span>
         <a className='govuk-link'>Ask HMRC online</a>
         <Button
