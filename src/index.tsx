d2882c7b974d6082012890570f53056236c46ebe
Stefan (CDIO) Sokolowski <U.7866928@columbus.local>
2023-08-17 17:10:15 +0100

    feat US-10513: i18n loadPath changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import TopLevelApp from './samples/TopLevelApp';
-import './i18n';
 import '../assets/css/appStyles.scss'
 
 const outletElement = document.getElementById("outlet");
--- src/samples/AppSelector/index.tsx
+++ src/samples/AppSelector/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 // import EmbeddedTopLevel from "../Embedded/EmbeddedTopLevel";
 import ChildBenefitsClaim from '../ChildBenefitsClaim/index';
 import CookiePage from '../ChildBenefitsClaim/CookiePage';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import i18n from 'i18next';
 
 const baseURL = "/";
 
 const AppSelector = () => {
+
+  const { pathname } = useLocation();
+
+  i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+      lng: sessionStorage.getItem('rsdk_locale')?.substring(0, 2) || 'en',
+      backend: {
+        /* translation file path */
+        loadPath: `${window.location.pathname.replace(pathname,'')}assets/i18n/{{lng}}.json`
+      },
+      fallbackLng: 'en',
+      debug: false,
+      returnNull: false,
+      react: {
+        useSuspense: false,
+      }
+    });
 
   return (
     <>
