5bc045cae2a2fae58d4df287a1a7f00b22a557e9
peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>
2023-09-14 12:42:29 +0100

    Re add useIsOnlyfield handler

--- src/components/helpers/hooks/QuestionDisplayHooks.ts
+++ src/components/helpers/hooks/QuestionDisplayHooks.ts
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 /**
  * Helper hook for handling instances where there is only one field presented in the current view.
  * Returns a boolean indicating whether or not there is only one field to display in the current context
@@ -6,6 +7,12 @@
 /* Retaining this code for future change in implementation of single question pages. */
 
 
-export default function useIsOnlyField(){
-    return false;
+export default function useIsOnlyField(effectTrigger = null){
+    const [isOnlyField, setisOnlyField] = useState(PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1").length === 1);
+
+  useEffect ( () => {
+    setisOnlyField(PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1").length === 1);
+  }, [effectTrigger])
+
+  return isOnlyField;
 }
