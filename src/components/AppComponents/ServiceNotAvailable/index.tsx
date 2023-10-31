8af1bcdac3390ad37377685c7550fc3420ce14b6
monalisasarkar123 <monalisa.sarkar@hmrc.gov.uk>
2023-10-31 13:34:41 +0000

    US-10099-2 UX review changes added

--- src/components/AppComponents/ServiceNotAvailable/index.tsx
+++ src/components/AppComponents/ServiceNotAvailable/index.tsx
@@ -4,17 +4,16 @@ import { useTranslation } from 'react-i18next';
 
 export default function ServiceNotAvailable(props) {
   const {returnToPortalPage } = props;
+  const mystyle = {
+  fontSize: "19px"
+  };
  
   const { t } = useTranslation();
-
- 
-
-
   return (
    
     <div className='govuk-body govuk-!-margin-bottom-9'>
          <h1 className="govuk-heading-l">{t('SERVICE_NOT_AVAILABLE')}</h1>
-         <p className="govuk-body">{t('COME_BACK_LATER')}</p>
+         <p className="govuk-body" style={mystyle}>{t('COME_BACK_LATER')}</p>
         <a href="#" className="govuk-link " onClick={returnToPortalPage} >{t('RETURN_TO_THE_HOMEPAGE')}</a>
     
                   
