ebca1c77fdcdbab11f82d970ee7d9b0390dbdfdb
Akhila <akhila.mineendsouza@hmrc.gov.uk>
2024-02-02 09:28:05 +0000

    resolved lint issue due to merge

--- src/components/helpers/formatters/ParsedHtml.tsx
+++ src/components/helpers/formatters/ParsedHtml.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
 import parse from 'html-react-parser';
 import DOMPurify from 'dompurify';
 
