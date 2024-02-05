b35e6400417c866b7405b82ee345dee888e665ad
Nick (CDIO) Waddington <U.7867438@columbus.local>
2024-02-05 10:19:35 +0000

    amended language toggle and app header

--- src/components/AppComponents/AppHeader/index.tsx
+++ src/components/AppComponents/AppHeader/index.tsx
@@ -80,7 +80,7 @@ export default function AppHeader(props) {
               </span>
             </p>
           </div>
-          {hasLanguageToggle && <LanguageToggle PegaApp={isPegaApp} />}
+          {hasLanguageToggle && <LanguageToggle />}
         </div>
       </header>
     </>
