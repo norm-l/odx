27db85325a38caed3245ef5192dd5c15888b57f1
Nick (CDIO) Waddington <U.7867438@columbus.local>
2024-05-01 17:24:40 +0100

    refactored with de-structuring

--- src/components/override-sdk/field/RichText/RichText.tsx
+++ src/components/override-sdk/field/RichText/RichText.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import ParsedHTML from '../../../helpers/formatters/ParsedHtml';
 
-export default function RichText(props) {
-  // Checking to see if the html includes a p tag.
-  // If so we can add the 'govuk-body' class to a div as below
-  const containsParagraphTag = props.value.indexOf('<p>') !== -1;
+export default function RichText({ value }) {
+  const containsParagraphTag = value.includes('<p>');
 
   return (
     <>
-      {containsParagraphTag ? (
+      {containsParagraphTag && (
         <div className='govuk-body'>
-          <ParsedHTML htmlString={props.value} />
+          <ParsedHTML htmlString={value} />
         </div>
-      ) : (
-        <ParsedHTML htmlString={props.value} />
       )}
+      {!containsParagraphTag && <ParsedHTML htmlString={value} />}
     </>
   );
 }
