768906f9827863ca08d3db34f10a707b373903ce
Nick (CDIO) Waddington <U.7867438@columbus.local>
2024-05-01 16:09:39 +0100

    added comments

--- src/components/override-sdk/field/RichText/RichText.tsx
+++ src/components/override-sdk/field/RichText/RichText.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ParsedHTML from '../../../helpers/formatters/ParsedHtml';
 
 export default function RichText(props) {
+  // Checking to see if the html includes a p tag.
+  // If so we can add the 'govuk-body' class to a div as below
   const containsParagraphTag = props.value.indexOf('<p>') !== -1;
 
   return (
