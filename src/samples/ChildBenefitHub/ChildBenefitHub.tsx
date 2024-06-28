9b951bdbfbc231dc8c8ba8f209d6a8b48166f70c
peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>
2024-06-28 09:42:40 +0100

    Hub page content added

--- src/samples/ChildBenefitHub/ChildBenefitHub.tsx
+++ src/samples/ChildBenefitHub/ChildBenefitHub.tsx
@@ -2,11 +2,15 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useStartMashup } from '../HighIncomeCase/reuseables/PegaSetup';
 import { loginIfNecessary, sdkIsLoggedIn } from '@pega/auth/lib/sdk-auth-manager';
+import AppHeader from '../../components/AppComponents/AppHeader';
+import AppFooter from '../../components/AppComponents/AppFooter';
+import { useTranslation } from 'react-i18next';
 
 
 export default function ChildBenefitHub(){
 
     const history = useHistory();
+    const { t } = useTranslation();
 
     const onRedirectDone = ()=>{
         history.push('/home');
@@ -20,7 +24,36 @@ export default function ChildBenefitHub(){
         }  
     }, [])
 
-    //useStartMashup(()=>{}, onRedirectDone, {});
 
-    return <>Child Benefit Hub</>
+    return ( sdkIsLoggedIn() ?
+    <>
+        <AppHeader hasLanguageToggle={true} /> 
+        <div className='govuk-width-container'>       
+         <main className="govuk-main-wrapper govuk-main-wrapper--l" id="main-content" role="main">
+            <div className="govuk-grid-row">
+            <div className="govuk-grid-column-two-thirds-from-desktop">
+                <h1 className="govuk-heading-xl">{t('CHB_HOMEPAGE_HEADING')}</h1>
+                <h2 className="govuk-heading-m">{t('CHB_HOMEPAGE_MAKING_CLAIM_SUBHEADING')}</h2>      
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.gov.uk/child-benefit">{t('CHB_HOMEPAGE_CHECK_IF_CAN_CLAIM_LINK')}</a></p>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.gov.uk/child-benefit/how-to-claim">{t('CHB_HOMEPAGE_MAKE_A_CLAIM_LINK')}</a></p>
+                <h2 className="govuk-heading-m">{t('CHB_HOMEPAGE_MANAGE_CLAIM_SUBHEADING')}</h2>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.gov.uk/report-changes-child-benefit">{t('CHB_HOMEPAGE_REPORT_CHANGES_LINK')}</a></p>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.gov.uk/child-benefit-16-19">{t('CHB_HOMEPAGE_CHILD_TURNS_16_GUIDANCE_LINK')}</a></p>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.tax.service.gov.uk/child-benefit/staying-in-education/extend-payments">{t('CHB_HOMEPAGE_EXTEND_PAYMENTS_LINK')}</a></p>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.tax.service.gov.uk/child-benefit/view-payment-history">{t('CHB_HOMEPAGE_VIEW_PAYMENT_HISTORY_LINK')}</a></p>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.tax.service.gov.uk/child-benefit/change-bank/change-account">{t('CHB_HOMEPAGE_CHANGE_BANK_DETAILS_LINK')}</a></p>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.tax.service.gov.uk/child-benefit/view-proof-entitlement">{t('CHB_HOMEPAGE_VIEW_PROOF_OF_ENTITLEMENT_LINK')}</a></p>
+                <p><a className="govuk-link" rel="noreferrer noopener" target="_blank" href="https://www.gov.uk/child-benefit-tax-charge">{t('CHB_HOMEPAGE_HICBC_CHARGE_LINK')}</a></p>
+                <p className="govuk-body govuk-!-margin-bottom-9"></p>
+            </div>
+            </div>
+            <a lang="en" className="govuk-link hmrc-report-technical-issue " rel="noreferrer noopener" target="_blank" href="https://www.tax.service.gov.uk/contact/report-technical-problem?newTab=true&service=PTA&referrerUrl=https%3A%2F%2Fwww.tax.service.gov.uk%2Fpersonal-account%2Fchild-benefit%2Fhome">{`${t('PAGE_NOT_WORKING_PROPERLY')} ${t("OPENS_IN_NEW_TAB")}`}</a>
+            
+            </main>        
+        </div>
+
+        <AppFooter />
+        
+    </> : <></>
+    )
 }
