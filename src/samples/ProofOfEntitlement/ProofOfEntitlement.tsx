ba717616b777edef32d9572ce363a3fd22d29d04
peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>
2024-07-01 13:46:14 +0100

    Initial PoE Screen

--- src/samples/ProofOfEntitlement/ProofOfEntitlement.tsx
+++ src/samples/ProofOfEntitlement/ProofOfEntitlement.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { loginIfNecessary, sdkIsLoggedIn } from '@pega/auth/lib/sdk-auth-manager';
+import AppHeader from '../../components/AppComponents/AppHeader';
+import AppFooter from '../../components/AppComponents/AppFooter';
+import { useTranslation } from 'react-i18next';
 
 
 export default function ProofOfEntitlement(){
 
     const history = useHistory();
+    const {t} = useTranslation();
 
     const onRedirectDone = ()=>{
         history.push('/view-proof-entitlement');
@@ -20,6 +24,21 @@ export default function ProofOfEntitlement(){
     }, [])
 
 
-    return <>Proof of Entitlement</>
+    return (
+        sdkIsLoggedIn() ? <>        
+            <AppHeader />
+            <div className='govuk-width-container'>
+                <main className="govuk-main-wrapper govuk-main-wrapper--l" id="main-content" role="main">
+                    <div className="govuk-grid-row">
+                        <div className="govuk-grid-column-two-thirds">
+                            <h1 className="govuk-heading-l">{t('PROOF_ENTITLEMENT_HEADING')}</h1>
+                        </div>
+                    </div>
+                </main>            
+            </div>
+            <AppFooter />
+        </> :
+        <></>
+    )
 }
 
