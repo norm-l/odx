8c2b6a085825c8bc70117db44e7ce808fe0fdcfc
Akhila <akhila.mineendsouza@hmrc.gov.uk>
2024-07-22 11:13:42 +0100

    Print POE - with hard coded NINO

--- src/samples/ProofOfEntitlement/ProofOfEntitlement.tsx
+++ src/samples/ProofOfEntitlement/ProofOfEntitlement.tsx
@@ -15,120 +15,198 @@ import Button from '../../components/BaseComponents/Button/Button';
 declare const PCore;
 declare const myLoadMashup: any;
 
-export default function ProofOfEntitlement(){
+export default function ProofOfEntitlement() {
+  const [entitlementData, setEntitlementData] = useState(null);
+  const [showNoAward, setShowNoAward] = useState(false);
+  const [showProblemWithService, setShowProblemWithService] = useState(false);
+  const [printView, setPrintView] = useState();
+  const history = useHistory();
+  const { t } = useTranslation();
 
-    const [entitlementData, setEntitlementData] = useState(null);
-    const [showNoAward, setShowNoAward] = useState(false);    
-    const [showProblemWithService, setShowProblemWithService] = useState(false);    
-    const history = useHistory();
-    const {t} = useTranslation();
+  registerServiceName(t('CHB_HOMEPAGE_HEADING'));
 
-    registerServiceName(t('CHB_HOMEPAGE_HEADING'));
+  const onRedirectDone = () => {
+    history.push('/view-proof-entitlement');
+    // appName and mainRedirect params have to be same as earlier invocation
+    loginIfNecessary({ appName: 'embedded', mainRedirect: true });
+  };
 
-    const onRedirectDone = ()=>{
-        history.push('/view-proof-entitlement');
-        // appName and mainRedirect params have to be same as earlier invocation
-        loginIfNecessary({ appName: 'embedded', mainRedirect: true });   
+  useEffect(() => {
+    if (!sdkIsLoggedIn()) {
+      loginIfNecessary({ appName: 'embedded', mainRedirect: true, redirectDoneCB: onRedirectDone });
     }
+    document.addEventListener('SdkConstellationReady', () => {
+      myLoadMashup('pega-root', false);
+      PCore.onPCoreReady(() => {
+        PCore.getDataPageUtils()
+          .getPageDataAsync('D_GetChBEntitlement', 'root', {
+            NINO: PCore.getEnvironmentInfo().getOperatorIdentifier()
+          })
+          .then(result => {
+            // If no claimant data in response, assume no award (or api error)
+            if (result.IsAPIError) {
+              setShowProblemWithService(true);
+            } else if (result.HasAward === false || !result.Claimant) {
+              setShowNoAward(true);
+            } else {
+              setEntitlementData(result);
+            }
+          })
+          .catch(() => {
+            setShowProblemWithService(true);
+          });
 
-    useEffect( () => {
-        if(!sdkIsLoggedIn()){
-            loginIfNecessary({ appName: 'embedded', mainRedirect: true, redirectDoneCB: onRedirectDone });
-            
-        }        
-        document.addEventListener('SdkConstellationReady', () => {
-            
-            myLoadMashup('pega-root', false);
-            PCore.onPCoreReady(() => {
-                PCore.getDataPageUtils()
-                .getPageDataAsync('D_GetChBEntitlement', 'root', {
-                    NINO: PCore.getEnvironmentInfo().getOperatorIdentifier()       
-                }, ).then((result) =>
-                {
-                    // If no claimant data in response, assume no award (or api error)
-                    if(result.IsAPIError){
-                        setShowProblemWithService(true);
-                    }
-                    else if(result.HasAward === false || !result.Claimant){
-                        setShowNoAward(true);                        
-                    } else {
-                        setEntitlementData(result);
-                    }
-                
-                }).catch(() => {
-                    setShowProblemWithService(true);
-                } )
-        })})
-    }, [])
-        
-    return (
-        <>
-            <AppHeader appname={t('CHB_HOMEPAGE_HEADING')} hasLanguageToggle handleSignout={sdkIsLoggedIn() ? triggerLogout : null} />
-            {sdkIsLoggedIn() && 
-            <div className='govuk-width-container'>
-            <MainWrapper>
-                            {entitlementData  &&  
-                                <>                 
-                                    <h1 className="govuk-heading-l">{t('PROOF_ENTITLEMENT_HEADING')}</h1>
+        PCore.getDataPageUtils()
+          .getPageDataAsync('D_GetChBEntitlementContent', 'root', {
+            NINO: 'YJ692820C'
+          })
+          .then(result => {
+            setPrintView(result.DocumentContentHTML);
+          })
+          .catch(err => {
+            // eslint-disable-next-line no-console
+            console.error(err);
+          });
+      });
+    });
+  }, []);
 
-                                    <Button variant='link' onClick={()=>{}}>{t('PRINT_THIS_PAGE')}</Button>
-                                    <p className='govuk-body'>{t('PROOF_ENTITLEMENT_CONFIRMATION')} {entitlementData.Claimant?.pyFullName} {t('ON')} {dayjs().format('D MMMM YYYY')}.</p>
-                                    <h2 className='govuk-heading-m'>{t('PROOF_ENTITLEMENT_DETAILS_H2')}</h2>
-                                    <p className='govuk-body'>{t('PROOF_ENTITLEMENT_IF_DETAILS_INCORRECT')}{' '}
-                                    <a href="https://www.gov.uk/report-changes-child-benefit/if-your-familys-circumstances-change" className="govuk-link" target="_blank" rel="noreferrer noopener">{t("PROOF_ENTITLEMENT_IF_DETAILS_INCORRECT_CHANGE_YOUR_CIRCUSMSTANCES_LINK")}{t("OPENS_IN_NEW_TAB")}</a>                          
-                                    {' '}{t("PROOF_ENTITLMENT_IF_DETAILS_INCORRECT_OR_ABOUT")}{' '}
-                                    <a href="https://www.gov.uk/report-changes-child-benefit" className="govuk-link" target="_blank" rel="noreferrer noopener">{t("PROOF_ENTITLEMENT_IF_DETAILS_INCORRECT_CHANGE_CHILDS_CIRCUMSTANCES_LINK")}{t("OPENS_IN_NEW_TAB")}</a>
-                                    {t("PROOF_ENTITLMENT_IF_DETAILS_INCORRECT_WILL_UPDATE")}
-                                    </p>
+  const printPOE = e => {
+    e.preventDefault();
+    const myWindow = window.open('');
+    myWindow.document.write(printView);
+    myWindow.print();
+  };
 
-                                    <dl className='govuk-summary-list'>
-                                        <ReadOnlyDisplay key='name' label={t('POE_LABEL_NAME')} value={entitlementData.Claimant?.pyFullName}/>                                                            
-                                        <ReadOnlyDisplay key='address' label={t('POE_LABEL_ADDRESS')} value={entitlementData.Claimant?.pyAddress} name={entitlementData.Claimant?.pyAddress.indexOf(',')?'CSV':''}/>                            
-                                        <ReadOnlyDisplay key='amount' label={t('POE_LABEL_AMOUNT')} value={`£${entitlementData.AwardValue?.toString()} ${t('PER_WEEK')}`}/>                            
-                                        <ReadOnlyDisplay key='startdate' label={t('POE_LABEL_START_DATE')} value={
-                                            `${entitlementData.IsMigrated?`${t('ON_OR_BEFORE')} `:''}${dayjs(entitlementData.AwardStart).format('D MMMM YYYY')}`
-                                        }/>                            
-                                        <ReadOnlyDisplay key='enddate' label={t('POE_LABEL_END_DATE')} value={dayjs(entitlementData.AwardEnd).format('D MMMM YYYY')
-                                        }/>  
-                                    </dl>
-                                    
-                                    { entitlementData.Children?.map((childData) => {
-                                        return(
-                                            <React.Fragment key={childData?.pyFullName}>
-                                                <h2 className='govuk-heading-m'>{t('CHILD_BENEFIT_DETAILS_FOR')} {childData?.pyFullName}</h2> 
-                                                <dl className='govuk-summary-list'>
-                                                    <ReadOnlyDisplay key={`${childData?.pyFullName} dob`} label={t('POE_LABEL_CHILD_DOB')} value={dayjs(childData.DateOfBirth).format('D MMMM YYYY') || ''}/>                            
-                                                    <ReadOnlyDisplay key={`${childData?.pyFullName} start`} label={t('POE_LABEL_START_DATE')} value={
-                                                        `${childData.IsMigrated?`${t('ON_OR_BEFORE')} `:''}${dayjs(childData.EligibilityStart).format('D MMMM YYYY')}`
-                                                    }/>                            
-                                                    <ReadOnlyDisplay key={`${childData?.pyFullName} end`} label={t('POE_LABEL_END_DATE')} value={dayjs(childData.EligibilityEnd).format('D MMMM YYYY')}/>  
-                                                </dl>
-                                            </React.Fragment>
-                                        )
-                                        })
-                                    }
+  return (
+    <>
+      <AppHeader
+        appname={t('CHB_HOMEPAGE_HEADING')}
+        hasLanguageToggle
+        handleSignout={sdkIsLoggedIn() ? triggerLogout : null}
+      />
+      {sdkIsLoggedIn() && (
+        <div className='govuk-width-container'>
+          <MainWrapper>
+            {entitlementData && (
+              <>
+                <h1 className='govuk-heading-l'>{t('PROOF_ENTITLEMENT_HEADING')}</h1>
 
-                                    <h2 className='govuk-heading-m'>{t('PROOF_ENTITLEMENT_VIEW_YOUR_PAYMENTS_H2')}</h2>
-                                    <a href="https://www.tax.service.gov.uk/child-benefit/view-payment-history" className="govuk-link">{t('PROOF_ENTITLEMENT_VIEW_PAST_PAYMENTS_LINK')}</a>                                    
-                                </> 
-                            }
-                            {
-                                showNoAward && <NoAwardPage />
-                            }
-                            {
-                                showProblemWithService && <>
-                                    <h1 className="govuk-heading-l">{t('SERVICE_NOT_AVAILABLE')}</h1>
-                                    <p className='govuk-body'>{t('COME_BACK_LATER')}</p>
-                                    <p className='govuk-body'>{t('YOU_WILL_NEED_TO_START_AGAIN')}</p>
-                                </>
-                            }
-                            <br/> 
-                            <br/> 
-                        </MainWrapper>
-                    </div>}
-        <AppFooter />
-        </>
-    
-        
-    )
+                <Button variant='link' onClick={e => printPOE(e)}>
+                  {t('PRINT_THIS_PAGE')}
+                </Button>
+                <p className='govuk-body'>
+                  {t('PROOF_ENTITLEMENT_CONFIRMATION')} {entitlementData.Claimant?.pyFullName}{' '}
+                  {t('ON')} {dayjs().format('D MMMM YYYY')}.
+                </p>
+                <h2 className='govuk-heading-m'>{t('PROOF_ENTITLEMENT_DETAILS_H2')}</h2>
+                <p className='govuk-body'>
+                  {t('PROOF_ENTITLEMENT_IF_DETAILS_INCORRECT')}{' '}
+                  <a
+                    href='https://www.gov.uk/report-changes-child-benefit/if-your-familys-circumstances-change'
+                    className='govuk-link'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    {t('PROOF_ENTITLEMENT_IF_DETAILS_INCORRECT_CHANGE_YOUR_CIRCUSMSTANCES_LINK')}
+                    {t('OPENS_IN_NEW_TAB')}
+                  </a>{' '}
+                  {t('PROOF_ENTITLMENT_IF_DETAILS_INCORRECT_OR_ABOUT')}{' '}
+                  <a
+                    href='https://www.gov.uk/report-changes-child-benefit'
+                    className='govuk-link'
+                    target='_blank'
+                    rel='noreferrer noopener'
+                  >
+                    {t('PROOF_ENTITLEMENT_IF_DETAILS_INCORRECT_CHANGE_CHILDS_CIRCUMSTANCES_LINK')}
+                    {t('OPENS_IN_NEW_TAB')}
+                  </a>
+                  {t('PROOF_ENTITLMENT_IF_DETAILS_INCORRECT_WILL_UPDATE')}
+                </p>
+
+                <dl className='govuk-summary-list'>
+                  <ReadOnlyDisplay
+                    key='name'
+                    label={t('POE_LABEL_NAME')}
+                    value={entitlementData.Claimant?.pyFullName}
+                  />
+                  <ReadOnlyDisplay
+                    key='address'
+                    label={t('POE_LABEL_ADDRESS')}
+                    value={entitlementData.Claimant?.pyAddress}
+                    name={entitlementData.Claimant?.pyAddress.indexOf(',') ? 'CSV' : ''}
+                  />
+                  <ReadOnlyDisplay
+                    key='amount'
+                    label={t('POE_LABEL_AMOUNT')}
+                    value={`£${entitlementData.AwardValue?.toString()} ${t('PER_WEEK')}`}
+                  />
+                  <ReadOnlyDisplay
+                    key='startdate'
+                    label={t('POE_LABEL_START_DATE')}
+                    value={`${entitlementData.IsMigrated ? `${t('ON_OR_BEFORE')} ` : ''}${dayjs(
+                      entitlementData.AwardStart
+                    ).format('D MMMM YYYY')}`}
+                  />
+                  <ReadOnlyDisplay
+                    key='enddate'
+                    label={t('POE_LABEL_END_DATE')}
+                    value={dayjs(entitlementData.AwardEnd).format('D MMMM YYYY')}
+                  />
+                </dl>
+
+                {entitlementData.Children?.map(childData => {
+                  return (
+                    <React.Fragment key={childData?.pyFullName}>
+                      <h2 className='govuk-heading-m'>
+                        {t('CHILD_BENEFIT_DETAILS_FOR')} {childData?.pyFullName}
+                      </h2>
+                      <dl className='govuk-summary-list'>
+                        <ReadOnlyDisplay
+                          key={`${childData?.pyFullName} dob`}
+                          label={t('POE_LABEL_CHILD_DOB')}
+                          value={dayjs(childData.DateOfBirth).format('D MMMM YYYY') || ''}
+                        />
+                        <ReadOnlyDisplay
+                          key={`${childData?.pyFullName} start`}
+                          label={t('POE_LABEL_START_DATE')}
+                          value={`${childData.IsMigrated ? `${t('ON_OR_BEFORE')} ` : ''}${dayjs(
+                            childData.EligibilityStart
+                          ).format('D MMMM YYYY')}`}
+                        />
+                        <ReadOnlyDisplay
+                          key={`${childData?.pyFullName} end`}
+                          label={t('POE_LABEL_END_DATE')}
+                          value={dayjs(childData.EligibilityEnd).format('D MMMM YYYY')}
+                        />
+                      </dl>
+                    </React.Fragment>
+                  );
+                })}
+
+                <h2 className='govuk-heading-m'>{t('PROOF_ENTITLEMENT_VIEW_YOUR_PAYMENTS_H2')}</h2>
+                <a
+                  href='https://www.tax.service.gov.uk/child-benefit/view-payment-history'
+                  className='govuk-link'
+                >
+                  {t('PROOF_ENTITLEMENT_VIEW_PAST_PAYMENTS_LINK')}
+                </a>
+              </>
+            )}
+            {showNoAward && <NoAwardPage />}
+            {showProblemWithService && (
+              <>
+                <h1 className='govuk-heading-l'>{t('SERVICE_NOT_AVAILABLE')}</h1>
+                <p className='govuk-body'>{t('COME_BACK_LATER')}</p>
+                <p className='govuk-body'>{t('YOU_WILL_NEED_TO_START_AGAIN')}</p>
+              </>
+            )}
+            <br />
+            <br />
+          </MainWrapper>
+        </div>
+      )}
+      <AppFooter />
+    </>
+  );
 }
