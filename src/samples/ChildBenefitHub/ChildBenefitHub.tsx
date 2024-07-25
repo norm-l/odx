410cefefd3ebaf1d7c54de48bbf50f946973116a
Akhila <akhila.mineendsouza@hmrc.gov.uk>
2024-07-25 11:23:37 +0100

    fix lint issue

--- src/samples/ChildBenefitHub/ChildBenefitHub.tsx
+++ src/samples/ChildBenefitHub/ChildBenefitHub.tsx
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next';
 import { registerServiceName } from '../../components/helpers/setPageTitleHelpers';
 import useHMRCExternalLinks from '../../components/helpers/hooks/HMRCExternalLinks';
 import { triggerLogout } from '../../components/helpers/utils';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import TimeoutPopup from '../../components/AppComponents/TimeoutPopup';
 import { initTimeout } from '../../components/AppComponents/TimeoutPopup/timeOutUtils';
 
