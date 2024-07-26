e3c9528c9fd5c2e1feef7cc0f718dfb8c47f6dfb
monalisasarkar123 <monalisa.sarkar@hmrc.gov.uk>
2024-07-26 09:23:16 +0100

    main pull

--- src/samples/EducationStart/Landing.tsx
+++ src/samples/EducationStart/Landing.tsx
@@ -12,7 +12,9 @@ export default function Landing({
   handleStartCliam,
   assignmentPConn,
   showPortalBanner,
-  setShowLandingPage
+  setShowLandingPage,
+  showPortalPageDefault,
+  setShowPortalPageDefault
 }) {
   const [inProgressClaims, setInProgressClaims] = useState([]);
   const [submittedClaims, setSubmittedClaims] = useState([]);
@@ -30,10 +32,10 @@ export default function Landing({
     return JSON.parse(childrenJSON.slice(childrenJSON.indexOf(':') + 1));
   }
 
-    const statusMapping = status => {
+  const statusMapping = status => {
     switch (status) {
       case 'Open-InProgress':
-        return { text: t('IN_PROGRESS'), tagColour: 'blue' };
+        return { text: t('IN_PROGRESS_1'), tagColour: 'blue' };
       case 'Pending-CBS':
       case 'Resolved-Completed':
       case 'Resolved-Rejected':
@@ -54,13 +56,13 @@ export default function Landing({
   function getClaims(data, buttonContent) {
     const claimsData = [];
     data.forEach(item => {
-      if (item.ClaimExtension?.Child?.pyFirstName !== null) {
         const claimItem = {
           claimRef: item.pyID,
           dateCreated: DateFormatter.Date(item.pxCreateDateTime, { format: 'DD MMM YYYY' }),
           dateUpdated: item.pxUpdateDateTime,
           children: [],
           actionButton: buttonContent,
+          rowDetails: { pzInsKey: item.pzInsKey, pyAssignmentID: item.pyAssignmentID },
           status: statusMapping(item.pyStatusWork)
         };
 
@@ -84,7 +86,6 @@ export default function Landing({
           });
         }
         claimsData.push(claimItem);
-      }
     });
     return claimsData;
   }
@@ -101,27 +102,18 @@ export default function Landing({
       .finally(() => setLoadingSubmittedClaims(false));
   }
 
-  function fetchInProgressClaimsData(isSaveComeBackClicked = false) {
+  function fetchInProgressClaimsData() {
     let inProgressClaimsData: any = [];
     // @ts-ignore
     PCore.getDataPageUtils()
       .getDataAsync('D_ClaimantWorkAssignmentEdStartCases', 'root')
       .then(resp => {
         resp = resp.data.slice(0, 10);
-        inProgressClaimsData = getClaims(resp, t('REQUESTS_IN_PROGRESS'));
+        inProgressClaimsData = getClaims(resp, t('CONTINUE_MY_REQUEST'));
         setInProgressClaims(inProgressClaimsData);
       })
       .finally(() => {
         setLoadingInProgressClaims(false);
-        if (isSaveComeBackClicked) {
-          // Here we are calling this close container because of the fact that above
-          // D_ClaimantWorkAssignmentChBCases API is getting excuted as last call but we want to make
-          // close container call as the very last one.
-          PCore.getContainerUtils().closeContainerItem(
-            PCore.getContainerUtils().getActiveContainerItemContext('app/primary'),
-            { skipDirtyCheck: true }
-          );
-        }
       });
   }
 
@@ -131,9 +123,10 @@ export default function Landing({
   }, []);
 
   return (
-    !(loadingInProgressClaims && loadingSubmittedClaims) && (
+    (!loadingInProgressClaims && !loadingSubmittedClaims) && (
       <>
-        {!showStartClaim && (inProgressClaims.length || submittedClaims.length) ? (
+        {showPortalPageDefault ||
+        (!showStartClaim && (inProgressClaims.length || submittedClaims.length)) ? (
           <PortalPage
             inProgressClaims={inProgressClaims}
             submittedClaims={submittedClaims}
@@ -141,6 +134,7 @@ export default function Landing({
             setShowStartClaim={setShowStartClaim}
             showPortalBanner={showPortalBanner}
             setShowLandingPage={setShowLandingPage}
+            setShowPortalPageDefault={setShowPortalPageDefault}
           />
         ) : (
           <StartClaim
