d3ae068c89e50b1297db702b56f23000d98d59b7
akshaykalaskar1 <akshay.kalaskar@hmrc.gov.uk>
2024-09-23 08:23:15 +0100

    test: Unit test setup

--- src/samples/TopLevelApp/index.tsx
+++ src/samples/TopLevelApp/index.tsx
@@ -1,27 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
 import { getSdkConfig } from '@pega/auth/lib/sdk-auth-manager';
 import AppSelector from '../AppSelector';
-
-
-// declare let __webpack_public_path__; // eslint-disable-line
 
 const TopLevelApp = () => {
   const [basepath, setBasepath] = useState('');  
   useEffect(() => {
     getSdkConfig().then(sdkConfig => {
       const url = new URL(sdkConfig.serverConfig.sdkContentServerUrl);
-      setBasepath(url.pathname);      
-      
-      
-      /* if(url){
-        // eslint-disable-next-line
-        __webpack_public_path__ = url;
-      } else {
-        // eslint-disable-next-line
-        __webpack_public_path__ = '/';
-      } */
+      setBasepath(url.pathname);
     });
   }, []);
 
