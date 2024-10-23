b9769f06bfb373b6425f8a31c88271966015420f
Nick (CDIO) Waddington <U.7867438@columbus.local>
2024-10-23 17:32:04 +0100

    started on layout of bespoke component

--- src/components/AppComponents/RemoveClaim/index.tsx
+++ src/components/AppComponents/RemoveClaim/index.tsx
@@ -1,45 +1,115 @@
-import React from 'react';
-// import { useTranslation } from 'react-i18next';
-import Button from '../../BaseComponents/Button/Button';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { getSdkConfig } from '@pega/auth/lib/sdk-auth-manager';
+import RadioButtons from '../../BaseComponents/RadioButtons/RadioButtons';
 
 export default function RemoveClaim({ showRemovePage }) {
-  function handleRemoveProcess() {
-    const url = 'https://journey-dt1.hmrc.gov.uk/prweb/app/chb-dev/api/application/v2';
-    const container = PCore.getContainerUtils().getActiveContainerItemName('app/primary');
+  const { t } = useTranslation();
 
-    const processUrl = PCore.getStoreValue(
-      '.href',
-      'caseInfo.availableProcesses[0].links.add',
-      container
-    );
+  const [errorMsg, setErrorMsg] = useState('');
 
-    const { invokeCustomRestApi } = PCore.getRestClient();
-    invokeCustomRestApi(
-      `${url}${processUrl}`,
-      {
-        method: 'POST',
-        body: '',
-        headers: '',
-        withoutDefaultHeaders: false
-      },
-      ''
-    )
-      .then(() => {
-        console.log('Removed Case Successfully');
+  const radioOptions = [
+    {
+      value: 'yes',
+      label: `${t('YES')}`
+    },
+    {
+      value: 'no',
+      label: `${t('NO')}`
+    }
+  ];
+
+  // Add user selection logic
+  async function handleRemoveProcess() {
+    const selectedOption = document.querySelector('input[name="removeTheClaim"]:checked');
+
+    if (selectedOption) {
+      const selectedOptionValue = selectedOption.getAttribute('value');
+      if (selectedOptionValue === 'yes') {
+        const sdkConfig = await getSdkConfig();
+        const url = 'https://journey-dt1.hmrc.gov.uk/prweb/app/chb-dev/api/application/v2';
+        // const url = `${sdkConfig.serverConfig.infinityRestServerUrl}/app/${sdkConfig.serverConfig.appAlias}`;
+        const container = PCore.getContainerUtils().getActiveContainerItemName('app/primary');
+
+        const processUrl = PCore.getStoreValue(
+          '.href',
+          'caseInfo.availableProcesses[0].links.add',
+          container
+        );
+
+        const { invokeCustomRestApi } = PCore.getRestClient();
+        invokeCustomRestApi(
+          `${url}${processUrl}`,
+          {
+            method: 'POST',
+            body: '',
+            headers: '',
+            withoutDefaultHeaders: false
+          },
+          ''
+        )
+          .then(() => {
+            console.log('Removed Case Successfully');
+            showRemovePage(false);
+            window.location.reload();
+          })
+          .catch(error => {
+            // handle the error
+            console.log(error);
+          });
+      } else {
         showRemovePage(false);
         window.location.reload();
-      })
-      .catch(error => {
-        // handle the error
-        console.log(error);
-      });
+      }
+    } else {
+      setErrorMsg('Please select an option');
+    }
   }
 
   return (
-    <div className='govuk-button-group govuk-!-padding-top-4'>
-      <Button type='button' onClick={() => handleRemoveProcess()}>
-        Remove Claim
-      </Button>
-    </div>
+    <>
+      <div className='govuk-grid-row'>
+        <div className='govuk-grid-column-two-thirds'>
+          <h1 className='govuk-heading-l'>Confirm and remove the current claim in progress </h1>
+
+          <form>
+            <dl className='govuk-summary-list'>
+              <div className='govuk-summary-list__row'>
+                <dt className='govuk-summary-list__key'>Created date</dt>
+                {/* translate the created date */}
+                <dd className='govuk-summary-list__value'>23 October 2024</dd>
+              </div>
+            </dl>
+            <RadioButtons
+              name='removeTheClaim'
+              displayInline
+              value=''
+              useSmallRadios
+              options={radioOptions}
+              label='Do you want to remove this claim?'
+              errorText=''
+            ></RadioButtons>
+            <button
+              className='govuk-button govuk-!-margin-top-30'
+              data-module='govuk-button'
+              onClick={handleRemoveProcess}
+              type='button'
+            >
+              {t('SAVE_AND_CONTINUE')}
+            </button>
+
+            <p>
+              <a className='govuk-link' href=''>
+                Save and come back later
+              </a>
+            </p>
+          </form>
+
+          <p>
+            <a href='#'>Is this page not working properly? (opens in new tab)</a>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
