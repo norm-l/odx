120c96e864a15f2386fc4638c9fbdb4bad46b87f
Nick (CDIO) Waddington <U.7867438@columbus.local>
2024-11-13 13:13:39 +0000

    first atttepmt

--- src/samples/ChildBenefitsClaim/index.tsx
+++ src/samples/ChildBenefitsClaim/index.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck - TypeScript type checking to be added soon
 import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import StoreContext from '@pega/react-sdk-components/lib/bridge/Context/StoreContext';
 import createPConnectComponent from '@pega/react-sdk-components/lib/bridge/react_pconnect';
@@ -158,6 +158,28 @@ export default function ChildBenefitsClaim() {
       setBeginNewClaimButtonForInProgressClaims(false);
     }
   }, [inprogressClaims]);
+
+  // Reset the page title if user clicks the backbutton
+  // if 'showUserPortal' then set pageTitle to...
+  // Use the set page title helper?
+  // import { useLocation } from 'react-router-dom';
+  const location = useLocation();
+
+  useEffect(() => {
+    const handlePopState = () => {
+      if (location.pathname === '/') {
+        document.title = 'Test';
+      }
+    };
+
+    // Listen for popstate (back button) events to ensure the title stays consistent
+    window.addEventListener('popstate', handlePopState);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [location.pathname]);
 
   function createCase() {
     displayPega();
